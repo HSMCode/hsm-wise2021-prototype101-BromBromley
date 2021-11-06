a751ffe01b89863eade63c7e0fa0c78343a1a91e
Mia <mia.wittenmeier@students.hs-mainz.de>
2021-11-06 13:59:06 +0100

    add scene prototype101; rename some assets

---
+++ Assets/Scripts/DebugTest.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugTest : MonoBehaviour
+{
+    // This script is just a test to try out the different debug log methods
+
+    void Awake()
+    {
+        Debug.LogWarning("Greetings, I am a debug warning, but you can ignore me.");
+    }
+    void Start()
+    {
+        Debug.Log("Hello, I'm a debug log!");
+        Debug.LogError("Salvete, error sum. Hoc modo temptamen est.");
+    }
+
+    void Update()
+    {
+        Debug.Log("Hi, I'm a debug log that appears every frame!");
+        Debug.LogWarning("Good day, I am a warning that appears every frame.");
+    }
+}
